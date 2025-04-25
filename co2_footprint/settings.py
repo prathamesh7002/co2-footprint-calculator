e2e865a9479c428ebbb9d5f7a1e7e2ca701789b3
prathamesh7002 <prathmeshsaharkar@gmail.com>
2025-04-25 23:12:21 +0530

    Update settings.py

--- co2_footprint/settings.py
+++ co2_footprint/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'co2_footprint.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
 
